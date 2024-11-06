ad6d29be478ae0f554b4a986bf932fb77659ea18
leerlandais <leerlandais@gmail.com>
2024-11-06 10:34:14 +0100

    admin comment : OK

--- src/Form/Comment1Type.php
+++ src/Form/Comment1Type.php
@@ -15,22 +15,22 @@ class Comment1Type extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('article_id')
-            ->add('user_id')
-            ->add('comment_date_created', null, [
-                'widget' => 'single_text',
-            ])
-            ->add('comment_username')
+        //    ->add('article_id')
+        //    ->add('user_id')
+        //    ->add('comment_date_created', null, [
+        //       'widget' => 'single_text',
+        //    ])
+        //    ->add('comment_username')
             ->add('comment_text')
             ->add('visible')
-            ->add('article', EntityType::class, [
-                'class' => Article::class,
-                'choice_label' => 'id',
-            ])
-            ->add('user', EntityType::class, [
-                'class' => User::class,
-                'choice_label' => 'id',
-            ])
+        //    ->add('article', EntityType::class, [
+        //        'class' => Article::class,
+        //        'choice_label' => 'id',
+        //    ])
+        //    ->add('user', EntityType::class, [
+        //        'class' => User::class,
+        //        'choice_label' => 'id',
+        //    ])
         ;
     }
 
