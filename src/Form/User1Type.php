ede94f2c31906f9b67675cd7ee6a69533207097e
leerlandais <leerlandais@gmail.com>
2024-11-07 08:25:06 +0100

    user creation : OK

--- src/Form/User1Type.php
+++ src/Form/User1Type.php
@@ -4,34 +4,28 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class UserType extends AbstractType
+class User1Type extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
             ->add('username')
-            ->add('roles', ChoiceType::class, [
-                'choices' => [
-                    'Super' => 'ROLE_SUPER',
-                    'Admin' => 'ROLE_ADMIN',
-                    'Redac' => 'ROLE_REDAC',
-                    'User' => 'ROLE_USER',
-                ],
-                'multiple' => true,
-                'expanded' => true,
-                'label' => 'Roles',
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'The password fields must match.',
             ])
-           // ->add('password')
             ->add('fullname')
-           // ->add('uniqid')
             ->add('email')
-            ->add('activate')
             ->add('img_loc')
             ->add('quote')
+            //  ->add('roles')
+            //  ->add('uniqid')
+            //  ->add('activate')
         ;
     }
 
